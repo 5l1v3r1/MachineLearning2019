77153158f09a5f05024843b6ddfc1d1e8356124c
dejonge00 <dejonge00@live.nl>
2019-12-12 12:24:48 +0100

    More plotting ad wavelets

--- readData.py
+++ readData.py
@@ -1,34 +1,76 @@
 import matplotlib.pyplot as plt
+import pywt
 
 files = ['abdomen1', 'abdomen2', 'abdomen3', 'thorax1', 'thorax2']
 
 
+# Data I/O
+def read_data(file_name: str):
+    with open(file_name + '.txt') as f:
+        return [float(x) for x in f.readlines()]
+
+
+# Plotting
 def add_plot(data: [float], max_len: int, ax: plt.Axes):
     ax.plot(range(min(max_len, len(data))), data[:min(len(data), max_len)])
 
 
-def plot_data(data: [[float]]):
+def plot_data(data: [[float]], title: str = ''):
     f, ax = plt.subplots(len(files))
+    f.suptitle(title)
+    plt.subplots_adjust(hspace=1)
     for i, name in zip(range(len(files)), files):
         add_plot(data[i], 2000, ax[i])
-    plt.show()
+        ax[i].set_title(name)
+    plt.draw()
 
 
-def read_data(file_name: str):
-    with open(file_name + '.txt') as f:
-        return [float(x) for x in f.readlines()]
+# Normalize input data
+def normalize_data(data):
+    mean = sum(data) / len(data)
+    data = [d - mean for d in data]
+    m = max(data)
+    return [d / m for d in data]
+
+
+# Wavelet transform
+def wavelet(data: [float], style: str = 'haar'):
+    a, d = pywt.dwt(data, style)
+    return a, d
+
+
+def inverse_wavelet(data: [float], detail: [float] = None, style: str = 'haar'):
+    return pywt.idwt(cA=data, cD=detail, wavelet=style)
 
 
 if __name__ == '__main__':
     data = [read_data(file_name) for file_name in files]
 
     # Plot given data
-    plot_data(data)
+    plot_data(data, 'Given data')
+
+    # Normalize data
+    data = [normalize_data(d) for d in data]
+    plot_data(data, 'Normalized data')
 
     # Wavelet transform
+    w_style = 'haar'
+    # w_style = 'bior1.1'
+    # w_style = 'db1'
+
+    data = [wavelet(d, w_style) for d in data]
 
     # Plot Transformed data
+    plot_data([x for x, _ in data], 'Wavelet ({}) adjusted data'.format(w_style))
+    plot_data([x for _, x in data], 'Wavelet detail data')
 
     # Transform back
+    without_detail = inverse_wavelet([d for d, _ in data], style=w_style)
+    with_detail = inverse_wavelet([d for d, _ in data], [d for _, d in data], style=w_style)
+    plot_data(without_detail, title='Re-transformed data ({}, without detail)'.format(w_style))
+    plot_data(with_detail, title='Re-transformed data ({}, with detail)'.format(w_style))
 
+    plot_data([a - b for a, b in zip(with_detail, without_detail)])
     # Plot output data
+
+    plt.show()
